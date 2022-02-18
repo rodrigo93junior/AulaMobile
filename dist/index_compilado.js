4c0916d019febd32de547a9182783d905658e843
rodrigo93junior <schunemannjr@hotmail.com>
2022-02-17 22:05:42 -0300

    AulaMobile

---
+++ dist/index_compilado.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const init = nome => {
+  console.log(nome);
+};
+
+init("Juareis");
+const pessoa = {
+  "olhos": "Castanhos",
+  "altura": "1,80",
+  "profissao": "Advogado",
+  "faleDeVoce": nome => {
+    console.log("Olá eu sou" + nome);
+  }
+};
+pessoa.faleDeVoce("Juana");
